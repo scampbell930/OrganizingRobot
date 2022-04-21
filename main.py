5a293a1acd050e9050f0e87af1952869671cfbb2
scampbell930 <66285083+scampbell930@users.noreply.github.com>
2022-04-21 15:56:20 -0600

    update

--- main.py
+++ main.py
@@ -69,22 +69,24 @@ def streamVisionSensor(sensor, clientID):
                 cv2.namedWindow("image")
                 cv2.imshow('image', img_rgb)
 
-                blue_image = img_rgb.copy()
+                annotate_image = img_rgb.copy()
                 red_image = img_rgb.copy()
 
                 # Load image into classifiers
-                blue_output = blue_classifier.detectMultiScale(blue_image)
+                blue_output = blue_classifier.detectMultiScale(annotate_image)
                 red_output = red_classifier.detectMultiScale(red_image)
 
                 # Draw rectangles on image
                 for (x, y, w, h) in blue_output:
-                    cv2.rectangle(blue_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.rectangle(annotate_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.putText(annotate_image, 'blue cube', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
                 for (x, y, w, h) in red_output:
-                    cv2.rectangle(red_image, (x, y), (x+w, y+h), (0, 255, 0), 1)
+                    cv2.rectangle(annotate_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.putText(annotate_image, 'red cube', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
-                cv2.imshow('Blue output', blue_image)
-                cv2.imshow('Red output', red_image)
+                cv2.imshow('Detection Output', annotate_image)
+                # cv2.imshow('Red output', red_image)
 
                 # Used for saving images and quitting
                 key = cv2.waitKey(1)
